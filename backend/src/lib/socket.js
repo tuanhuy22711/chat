fcf4d5f314c8994f6e70703229174ade62cbf562
Tran Tuan Huy <trantuanhuy@Mac-mini-cua-Tran.local>
2025-08-08 21:25:43 +0700

    update

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: "*", // Cho phép tất cả domain
+    methods: ["GET", "POST"], // Nên khai báo method được phép
   },
 });
 
